9476e44412a0c63b1d37ae2ecdd93a606804d1bc
akmalnawabi <akmalnawabi007@gmail.com>
2025-12-13 11:12:25 +0330

    new

--- dcrm_website/models.py
+++ dcrm_website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
