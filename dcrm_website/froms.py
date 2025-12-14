486acbd17a73a1cf4b74fa7304bc57a5a914ce6d
akmalnawabi <akmalnawabi007@gmail.com>
2025-12-14 13:35:04 +0330

    final

--- dcrm_website/froms.py
+++ dcrm_website/froms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'firstname'}))
@@ -29,3 +30,14 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(max_length=100, required=True)
+    address = forms.CharField(max_length=100, required=False)
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
--- dcrm_website/views.py
+++ dcrm_website/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .froms import SignUpForm
+from .froms import SignUpForm, AddRecordForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -19,7 +21,7 @@ def home(request):
             messages.success(request, "there was an error, please try again")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout(request)
@@ -42,3 +44,49 @@ def register(request):
         return render(request, 'register.html', {"form":form})
 
     return render(request, "register.html", {"form": form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to do that")
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record updated successfully")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form, 'current_record': current_record})
+    else:
+        messages.success(request, "You must be logged in to do that")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            # process form data
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record added successfully")
+                return redirect('home')
+
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in to do that")
+        return redirect('home')
