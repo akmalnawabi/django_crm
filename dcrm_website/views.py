2c78848a03e3b8d13b5e832226d1823f6d18c49c
akmalnawabi <akmalnawabi007@gmail.com>
2025-12-14 14:43:20 +0330

    n

--- dcrm_website/views.py
+++ dcrm_website/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .froms import SignUpForm, AddRecordForm
 from .models import Record
 
-# Create your views here.
 def home(request):
     records = Record.objects.all()
     if request.method == 'POST':
